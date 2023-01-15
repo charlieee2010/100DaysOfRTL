990ba9dc43c59314f8cf4fbc0389357c7d37eb7a
charlieee2010 <76043675+charlieee2010@users.noreply.github.com>
2023-01-15 11:29:05 +0200

    Update tb_day5.v

--- Day5/tb_day5.v
+++ Day5/tb_day5.v
@@ -1,16 +1,39 @@
 
 module tb_day5();
-   reg clk,reset;
+   reg clk;
+   reg reset;
    wire [7:0] count;
 
-   day5 DUT(.clk(clk),.reset(reset),.count(count));
+   day5 dut(clk, reset, count);
 
    initial begin
+      //Initialize inputs and waveform
       clk = 0;
       reset = 1;
-      #5;
+      #10
       reset = 0;
-      $monitor("time = %0d, count = %d",$time, count); //monitor the output
-      forever #5 clk = ~clk;
+      #10
+      //Increment the counter 8 times
+      repeat(8) begin
+         clk = 1;
+         #5
+         clk = 0;
+         #5
+      end 
+   end
+
+   initial begin
+      //Check if the count is correct
+      if(count == 8'h8)
+        $display("Test passed!");
+      else
+        $display("Test failed!");
+      #10
+      $finish;
+   end
+   initial begin
+      $dumpfile("day5.vcd");
+      $dumpvars();
    end
 endmodule
+
